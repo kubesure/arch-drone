5689264baaf75aef96b74461771c1896f01f4ea9
Viral Gohil <viral.v21@gmail.com>
2023-10-11 20:23:17 +0400

    Red detection handle with multiple ranges

--- detector/contour.py
+++ detector/contour.py
@@ -4,50 +4,60 @@ from drone_types import RingColor, Ring
 
 
 class ContourFilter:
+
     def __init__(self):
         pass
-
     def empty(self, a):
         pass
 
     # Get Ring Color Range in Numpy
     def get_ring_hsv(self, ring: RingColor):
-        lower = None
-        upper = None
+        lower = []
+        upper = []
         known_width = 0
         iteration = 0
         if ring == RingColor.RED:
-            lower = np.array([0, 100, 100])
-            upper = np.array([5, 255, 255])
+            lower.append(np.array([0, 100, 100], dtype=np.uint8))
+            upper.append(np.array([5, 255, 255], dtype=np.uint8))
+            lower.append(np.array([175, 100, 100], dtype=np.uint8))
+            upper.append(np.array([180, 255, 255], dtype=np.uint8))
             known_width = 560
-            iteration = 6
+            iteration = 3
         elif ring == RingColor.YELLOW:
-            lower = np.array([20, 100, 100])
-            upper = np.array([30, 255, 255])
+            lower.append(np.array([20, 100, 100], dtype=np.uint8))
+            upper.append(np.array([30, 255, 255], dtype=np.uint8))
             known_width = 480
             iteration = 1
+
         return lower, upper, known_width, iteration
+
 
     # Get the filter contour from image
     def get_xyz_ring(self, img, ring: RingColor):
 
         closure_curve = True
-        FOCAL_LENGTH = 42
+        focal_length = 42
 
-        lower, upper, known_width, iteration = self.get_ring_hsv(ring)
-        imgHsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        lowers, uppers, known_width, iteration = self.get_ring_hsv(ring)
+        img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-        mask = cv2.inRange(imgHsv, lower, upper)
+        mask = None
+        for lower, upper in zip(lowers, uppers):
+            if mask is None:
+                mask = cv2.inRange(img_hsv, lower, upper)
+            else:
+                mask = cv2.bitwise_or(mask, cv2.inRange(img_hsv, lower, upper))
+
         result = cv2.bitwise_and(img, img, mask=mask)
         # mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
 
-        imgBlur = cv2.GaussianBlur(result, (3, 3), 0, borderType=cv2.BORDER_CONSTANT)
-        imgGray = cv2.cvtColor(imgBlur, cv2.COLOR_BGR2GRAY)
-        imgCanny = cv2.Canny(imgGray, 166, 175)
-        kernel = np.ones((5, 5), "uint8")
-        imgDil = cv2.dilate(imgCanny, kernel, iterations=iteration)
+        img_blur = cv2.GaussianBlur(result, (3, 3), 0, borderType=cv2.BORDER_CONSTANT)
+        img_gray = cv2.cvtColor(img_blur, cv2.COLOR_BGR2GRAY)
+        img_canny = cv2.Canny(img_gray, 166, 175)
+        kernel = np.ones((5, 5), dtype=np.uint8)
+        img_dil = cv2.dilate(img_canny, kernel, iterations=iteration)
 
-        contours, _ = cv2.findContours(imgDil, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        contours, _ = cv2.findContours(img_dil, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
         center_x = 0
         center_y = 0
         distance = 0
@@ -68,7 +78,7 @@ class ContourFilter:
                 x, y, bounding_rect_width, bounding_rect_height = cv2.boundingRect(approx)
                 center_x = int(x + (bounding_rect_width / 2))  # CENTER X OF THE OBJECT
                 center_y = int(y + (bounding_rect_height / 2))  # CENTER y OF THE OBJECT
-                distance = self.distance_to_camera(known_width, FOCAL_LENGTH, bounding_rect_width)
+                distance = self.distance_to_camera(known_width, focal_length, bounding_rect_width)
 
                 cv2.circle(img, (int(center_x), int(center_y)), 3, (0, 0, 0), -1)
                 cv2.rectangle(img, (x, y), (x + bounding_rect_width, y + bounding_rect_height), (0, 255, 0), 5)
@@ -81,9 +91,9 @@ class ContourFilter:
         # error should only return of collect in if condition
         center_x = int(center_x / 10)
         r = Ring(x=center_x, y=center_y, z=distance, area=area, color=ring)
-        return r, img
+        return r, img, result, img_canny, img_dil
 
     # compute and return the distance from the maker to the camera
-    def distance_to_camera(self, knownWidth, focalLenght, perceivedWidth):
-        distance = ((knownWidth * focalLenght) / perceivedWidth) * 2.54
+    def distance_to_camera(self, known_width, focal_length, perceived_width):
+        distance = ((known_width * focal_length) / perceived_width) * 2.54
         return int(distance)
